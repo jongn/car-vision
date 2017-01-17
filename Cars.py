63d8eef50e53d4cf0ecc156b01241a0de7767cf4
Jonathan Ngan <jongan@berkeley.edu>
2017-01-17 13:35:04 -0800

    recorded sample output

--- Cars.py
+++ Cars.py
@@ -58,6 +58,7 @@ def background_test(bg, initial_learning):
             frame_count = frame_count + 1
         else:
             break
+
     back.release()
     background = cv2.convertScaleAbs(avg)
     cv2.imshow('background',background)
@@ -72,30 +73,42 @@ def background_test(bg, initial_learning):
         if ret:
             fgmask = bg.apply(frame)
 
+
+
             closing = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel)
             opening = cv2.morphologyEx(closing, cv2.MORPH_OPEN, kernel)
             dilation = cv2.dilate(opening, kernel, iterations=1)
 
+
             (_, contours, _) = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
             #matches = []
+            
             for (i, contour) in enumerate(contours):
                 (x, y, w, h) = cv2.boundingRect(contour)
-                contour_valid = (w >= 6) and (h >= 6)
+                contour_valid = (w >= 10) and (h >= 10)
 
                 if not contour_valid:
                     continue
-
-                centroid = get_centroid(x, y, w, h)
-                cv2.rectangle(fgmask, (x,y), (x+w,y+h),(255,0,0),2)
-                cv2.rectangle(frame, (x,y), (x+w,y+h),(255,0,0),2)
+            
+                #centroid = get_centroid(x, y, w, h)
+                #cv2.rectangle(fgmask, (x,y), (x+w,y+h),(255,0,0),2)
+                #cv2.rectangle(frame, (x,y), (x+w,y+h),(255,0,0),2)
                 #matches.append(((x, y, w, h), centroid))
 
             cv2.imshow('mask', fgmask)
             cv2.imshow('dilation', dilation)
-            cv2.imshow('oritinal', frame)
+            cv2.imshow('original', frame)
 
-            cv2.waitKey(300)
+
+            orig_string = 'Output/' + str(frame_id) + '_orig.jpg'
+            mask_string = 'Output/' + str(frame_id) + '_mask.jpg'
+            dil_string = 'Output/' + str(frame_id) + '_dilation.jpg'
+            cv2.imwrite(orig_string, frame)
+            cv2.imwrite(mask_string, fgmask)
+            cv2.imwrite(dil_string, dilation)
+
+            cv2.waitKey(10)
             frame_id += 1
 
         else:
--- Utils.py
+++ Utils.py
@@ -10,7 +10,7 @@ def grab_image(url, img_name):
 def grab_image_1min(url, img_name):
 	frame = 0
 	while True:
-		image_name = str(frame) + "_" + str(frame) + "_" + img_name
+		image_name = str(frame) +  "_" + img_name
 		grab_image(url, image_name)
 		time.sleep(60)
 		frame = frame + 1
@@ -19,7 +19,6 @@ def main():
 	url = "http://www.dot.ca.gov/cwwp2/data/d4/cctv/image/TV388_N1PRESIDIO.jpg?1378316522948"
 	img_name = "TV388_N1PRESIDIO.jpg"
 	grab_image_1min(url, img_name)
-
 
 
 if __name__ == "__main__":
